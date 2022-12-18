0307a52a77bf663029cdb52851e2ac54c202b05b
JonathanMitchell1234 <88846650+JonathanMitchell1234@users.noreply.github.com>
2022-12-18 16:34:44 -0500

    added MaterialUI loading button and added loading prop

--- src/API.js
+++ src/API.js
@@ -1,51 +1,65 @@
-import React, {useState } from 'react';
-import axios from 'axios';
-import { Button } from '@mui/material';
-
-
+import React, { useState } from "react";
+import axios from "axios";
+import { Button } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 function MyComponent() {
-  const [htmlUrl, setHtmlUrl] = useState('');
-  const [pdfUrl, setPdfUrl] = useState('');
+	const [htmlUrl, setHtmlUrl] = useState("");
+	const [pdfUrl, setPdfUrl] = useState("");
+	const [isLoading, setIsLoading] = useState(false); // Add this line to add the isLoading state
 
-  const handleChange = (event) => {
-    setHtmlUrl(event.target.value);
-  };
+	const handleChange = (event) => {
+		setHtmlUrl(event.target.value);
+	};
 
-  const handleGeneratePdf = () => {
-    axios
-		.post(
-			"https://api.html2pdf.app/v1/generate",
-			{
-				html: `<iframe src="${htmlUrl}" height="1000%" width="100%" />`,
-				apiKey: "thDFo86J5Cs2cLdGvnCTuiMEAfqtb79GG0AETyGbsrpMAfTxc7IwPwkYzHK3n3wl",
-			},
-			{ responseType: "arraybuffer" }
-		)
-		.then((response) => {
-			const blob = new Blob([response.data], { type: "application/pdf" });
-			const url = URL.createObjectURL(blob);
-			setPdfUrl(url);
-		})
-		.catch((err) => {
-			console.log(err.message);
-		});
-  };
+	const handleGeneratePdf = () => {
+		setIsLoading(true); // Set isLoading to true when the button is clicked
 
-  return (
-    <div>
-      <input type="text" className='linkField' value={htmlUrl} onChange={handleChange} /> <br />
-      <Button variant="contained" onClick={handleGeneratePdf}>Generate PDF</Button>
-      {pdfUrl && (
-        <div>
-          <a href={pdfUrl} target="_blank" rel="noopener noreferrer">View PDF</a>
-          <br />
-          <Button variant="contained" onClick={() => setPdfUrl('')}>Clear PDF</Button>
-        </div>
-      )}
-    </div>
-  );
+		axios
+			.post(
+				"https://api.html2pdf.app/v1/generate",
+				{
+					html: `<iframe src="${htmlUrl}" height="1000%" width="100%" />`,
+					apiKey: "thDFo86J5Cs2cLdGvnCTuiMEAfqtb79GG0AETyGbsrpMAfTxc7IwPwkYzHK3n3wl",
+				},
+				{ responseType: "arraybuffer" }
+			)
+			.then((response) => {
+				const blob = new Blob([response.data], { type: "application/pdf" });
+				const url = URL.createObjectURL(blob);
+				setPdfUrl(url);
+				setIsLoading(false); // Set isLoading to false when the action is complete
+			})
+			.catch((err) => {
+				console.log(err.message);
+				setIsLoading(false); // Set isLoading to false if there is an error
+			});
+	};
+
+	return (
+		<div>
+			<input type="text" className="linkField" value={htmlUrl} onChange={handleChange} /> <br />
+			<LoadingButton
+				loading={isLoading} // Use the isLoading state to control the loading state of the button
+				loadingIndicator="Loading…"
+				variant="outlined"
+				onClick={handleGeneratePdf}
+			>
+				Generate PDF
+			</LoadingButton>
+			{pdfUrl && (
+				<div>
+					<a href={pdfUrl} target="_blank" rel="noopener noreferrer">
+						View PDF
+					</a>
+					<br />
+					<Button variant="contained" onClick={() => setPdfUrl("")}>
+						Clear PDF
+					</Button>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default MyComponent;
-
